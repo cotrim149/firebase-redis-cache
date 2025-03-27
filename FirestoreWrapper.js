144197071c655df48c80d69132452b28fe688f28
Victor Cotrim <cotrim149@gmail.com>
2025-03-27 19:45:10 +0000

    Adding method to create and edit collections

--- FirestoreWrapper.js
+++ FirestoreWrapper.js
@@ -1,4 +1,4 @@
-const { collection, getDocs } = require("firebase/firestore");
+const { collection, getDocs, doc, updateDoc, writeBatch } = require("firebase/firestore");
 
 class FirestoreWrapper {
   constructor(firestore) {
@@ -12,6 +12,46 @@ class FirestoreWrapper {
       dataList.push({ id: doc.id, ...doc.data() });
     });
     return dataList;
+  }
+
+  async setCollection(collectionName, collectionData) {
+
+    if (!Array.isArray(collectionData)) {
+      throw new Error("Os dados da coleção devem ser um array de objetos.");
+    }
+
+    const batch = writeBatch(this.db);
+    const colRef = collection(this.db, collectionName);
+
+    for (const item of collectionData) {
+      if (item.id) {
+        // Caso o item já tenha um ID, usamos ele
+        const docRef = doc(this.db, collectionName, item.id);
+        batch.set(docRef, item);
+      } else {
+        // Se não tiver um ID, criamos um novo documento
+        await addDoc(colRef, item);
+      }
+    }
+
+    await batch.commit();
+    console.log(`✅ Coleção "${collectionName}" salva com sucesso.`);
+  }
+
+  async editCollection(collectionName, updateFunction) {
+    const colRef = collection(this.db, collectionName);
+    const snapshot = await getDocs(colRef);
+
+    for (const document of snapshot.docs) {
+      const register = { id: document.id, ...document.data() };
+      const updatedSale = await updateFunction(register);
+
+      if (updatedSale) {
+        const docRef = doc(this.db, collectionName, document.id);
+        await updateDoc(docRef, updatedSale.data);
+        console.log(`✅ Documento ${document.id} atualizado!`);
+      }
+    }
   }
 }
 
